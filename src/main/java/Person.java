5ce844f72ea288b4f95bba9d6329a9c0ebe15e9f
BardaK03 <bardadarius6@gmail.com>
2023-12-04 18:50:20 +0200

    Initial commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,20 @@
+
+public class Person {
+    private String Name;
+    private String Clasa;
+
+    public Person(String Name, String Clasa)
+    { this.Name=Name;
+        this.Clasa=Clasa;
+    }
+
+    public String getName(){
+        return Name;
+    }
+    public void setName(String name){
+        Name = name;
+    }
+    public String getClasa{
+
+    }
+}
